7c97d88fa07c91b1edcd4d6c1ebd4b767e2976b2
khantahseen <tahseen@promactinfo.com>
2020-09-08 12:23:05 +0530

    Initialised the Users

--- EmployeeManagementSystem/EmployeeManagementSystem/Controllers/HomeController.cs
+++ EmployeeManagementSystem/EmployeeManagementSystem/Controllers/HomeController.cs
@@ -6,22 +6,82 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using EmployeeManagementSystem.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace EmployeeManagementSystem.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly RoleManager<IdentityRole> roleManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,AppDbContext context, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, RoleManager<IdentityRole> roleManager)
         {
             _logger = logger;
+            _context = context;
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.roleManager = roleManager;
         }
 
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+
+            var users = userManager.Users;
+            if (users != null)
+            {
+                return View();
+           }
+            else
+            {
+                
+                var adminName = "tash@gmail.com";
+                var adminEmail = "tash@gmail.com";
+                var adminPassword = "Tash@123";
+                var user = new IdentityUser
+                {
+                    UserName = adminName,
+                    Email = adminEmail
+                };
+                var result = await userManager.CreateAsync(user, adminPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "Admin");
+                    return RedirectToAction("Index");
+                }
+
+                
+                var hrName = "taif@gmail.com";
+                var hrEmail = "taif@gmail.com";
+                var hrPassword = "Taif@123";
+                var user2 = new IdentityUser
+                {
+                    UserName = hrName,
+                    Email = hrEmail
+                };
+                var result2 = await userManager.CreateAsync(user2, hrPassword);
+
+                if (result2.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user2, "HR");
+                    return RedirectToAction("Index");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View();
+           }
+
         }
+
 
         public IActionResult Privacy()
         {
